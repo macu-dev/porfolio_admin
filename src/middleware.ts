6d69cf890111ee1558ae88d5be4dcf75bc7d5228
Maria Claudia <mariaclaudiaperezes@gmail.com>
2024-02-25 22:34:54 -0300

    feat(auth): add auth + internalization

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,39 @@
 import { i18nRouter } from 'next-i18n-router';
-import { NextRequest } from 'next/server';
+import { NextFetchEvent, NextRequest } from 'next/server';
+import { NextRequestWithAuth, withAuth } from 'next-auth/middleware';
 
-import i18nConfig from '../i18nConfig';
+import i18nConfig, { locales } from '../i18nConfig';
 
-export function middleware(request: NextRequest) {
-  return i18nRouter(request, i18nConfig);
+const publicPages = ['/', '/admin'];
+
+const authMiddleware = withAuth((req) => i18nRouter(req, i18nConfig), {
+  secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    authorized: async ({ token }) => !!token,
+  },
+  pages: {
+    signIn: '/admin',
+  },
+});
+
+export async function middleware(req: NextRequest, event: NextFetchEvent) {
+  const publicPathnameRegex = RegExp(
+    `^(/(${locales.join('|')}))?(${publicPages
+      .flatMap((p) => (p === '/' ? ['', '/'] : p))
+      .join('|')})/?$`,
+    'i',
+  );
+
+  const isPublicPage = publicPathnameRegex.test(req.nextUrl.pathname);
+
+  if (isPublicPage) {
+    return i18nRouter(req, i18nConfig);
+  } else {
+    return authMiddleware(req as NextRequestWithAuth, event);
+  }
 }
 
 // only applies this middleware to files in the app directory
 export const config = {
-  matcher: '/((?!api|static|.*\\..*|_next).*)',
+  matcher: ['/((?!api|static|.*\\..*|_next).*)'],
 };
