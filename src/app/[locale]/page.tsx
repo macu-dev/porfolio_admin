c6968139b9f9fb29e877aea561c470d7135db1c8
Maria Claudia <mariaclaudiaperezes@gmail.com>
2024-03-05 21:55:57 -0300

    feat(components): add header

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,9 +1,17 @@
-import Header from '@/components/ui/header';
+import { Header } from '@/components/ui/header';
 
 export default function Home() {
   return (
     <>
-      <Header />
+      <Header
+        image={
+          <img
+            alt="Flowbite Logo"
+            className="mr-3 h-6 sm:h-9"
+            src="https://flowbite.com/docs/images/logo.svg"
+          />
+        }
+      />
       <main className="container mx-auto h-full py-2">
         <p>sdsd</p>
       </main>
