349b980802c270f15db7da64f269e15bcfda800e
Maria Claudia <mariaclaudiaperezes@gmail.com>
2024-08-10 23:21:13 -0300

    feat(src): add service

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 // import CursorAura from '@/components/domain/CursorAura';
 import SocialLinks from '@/components/domain/SocialLinks';
-import { fetchData } from '@/services/api';
+import { serviceApi } from '@/services/api';
 
 import initTranslations from '../i18n';
 
@@ -12,10 +12,7 @@ export default async function Home({
   params: { locale: string };
 }) {
   const { t } = await initTranslations(locale, ['about']);
-  const data = await fetchData(`/api/about-me-page?locale=${locale}`);
-
-  // eslint-disable-next-line no-console
-  console.log(data);
+  const data = await serviceApi.getAboutMe({ locale });
 
   return (
     <>
@@ -24,13 +21,13 @@ export default async function Home({
           <div className="flex h-full flex-col items-center  justify-evenly max-[1024px]:justify-center sm:px-8  lg:flex-row">
             <div className="animate-slide-up-text max-[1023px]:order-2 lg:max-w-screen-sm">
               <p className="text-default-500 my-2 block w-full max-w-full text-lg font-normal md:w-1/2 min-[768px]:text-[2.1rem] lg:text-xl">
-                {t('pretitle')}
+                {data.pretitle}
               </p>
               <h1 className="inline bg-gradient-text bg-clip-text text-[2.5rem] font-bold leading-[1.2] tracking-tight text-transparent min-[768px]:text-[4rem] lg:text-7xl">
-                Maria Claudia <br /> Perez Escalante
+                {data.title}
               </h1>
               <h2 className="mt-3 text-[1.5rem] font-semibold min-[768px]:text-[2rem] lg:text-2xl">
-                Frontend developer
+                {data.subtitle}
               </h2>
               <p className="my-6 leading-[1.8] text-stone-300 max-[1024px]:my-3  max-[1024px]:text-[1.1rem] min-[768px]:text-[1.6rem] lg:max-w-xl lg:text-lg">
                 {t('text')}
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,16 +1,69 @@
+/* eslint-disable no-console */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { flattenAttributes, getStrapiURL } from '@/lib/utils';
 
 const baseUrl = getStrapiURL();
 
-export async function fetchData(url: string) {
+interface AboutMeArgs {
+  locale: string;
+}
+
+interface AboutMeData {
+  title: string;
+  pretitle: string;
+  description: { type: string; children: { type: string; text: string }[] }[];
+  subtitle: string | null;
+}
+
+type ApiResponses = {
+  AboutMe: AboutMeData;
+};
+
+type ApiArgs = {
+  AboutMe: AboutMeArgs;
+};
+
+type ServiceApi = {
+  [K in keyof ApiResponses as `get${K}`]: (
+    args: ApiArgs[K],
+  ) => Promise<ApiResponses[K]>;
+};
+
+export async function fetchData<T>(url: string): Promise<T> {
   try {
     const response = await fetch(`${baseUrl}${url}`);
     const data = await response.json();
 
-    return flattenAttributes(data);
+    return flattenAttributes(data) as T;
   } catch (error) {
-    // eslint-disable-next-line no-console
     console.error('Error fetching data:', error);
-    throw error; // or return null;
+    throw error;
   }
 }
+
+const apiUrls: { [K in keyof ApiArgs]: (args: ApiArgs[K]) => string } = {
+  AboutMe: ({ locale }: AboutMeArgs) => `/api/about-me-page?locale=${locale}`,
+};
+
+const initServiceApi = (): ServiceApi => {
+  const services = {} as ServiceApi;
+
+  for (const key in apiUrls) {
+    if (Object.prototype.hasOwnProperty.call(apiUrls, key)) {
+      const endpointKey = key as keyof ApiResponses;
+
+      services[`get${endpointKey}` as keyof ServiceApi] = async (
+        args: any,
+      ): Promise<any> => {
+        return fetchData<ApiResponses[typeof endpointKey]>(
+          apiUrls[endpointKey](args),
+        );
+      };
+    }
+  }
+
+  return services;
+};
+
+// Exporta el servicio de API
+export const serviceApi = initServiceApi();
